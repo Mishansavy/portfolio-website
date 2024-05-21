ede1eb4cf6973ab129009d28e69f91b857a1d22e
Mishansavy <smishan7@gmail.com>
2024-05-21 09:56:03 +0545

    portfolio website completed

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -21,8 +21,20 @@ const iconvariants = (duration) => ({
 export const Technologies = () => {
   return (
     <div className="border-b border-gray-900	pb-24">
-      <h1 className="my-10 text-center text-4xl">Technologies</h1>
-      <div className="flex flex-wrap items-center justify-center gap-2">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-2"
+      >
         {/* reactjs  */}
         <motion.div
           variants={iconvariants(2.5)}
@@ -68,7 +80,7 @@ export const Technologies = () => {
         >
           <TbBrandNextjs className="text-7xl " />
         </motion.div>
-      </div>
+      </motion.div>
     </div>
   );
 };
