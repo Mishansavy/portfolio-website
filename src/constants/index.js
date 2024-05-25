73063f802afbd030cb35535cd8d7600260172bbf
Mishansavy <smishan7@gmail.com>
2024-05-25 13:28:50 +0545

    corrected contact info

--- src/constants/index.js
+++ src/constants/index.js
@@ -57,7 +57,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Kalanki, Kathmandu, Nepal ",
+  phoneNo: "+977- 9862276734 ",
+  email: "smishan7@gmail.com",
 };
